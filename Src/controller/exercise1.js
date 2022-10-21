51bae4cf195c67f9fae141b61d2156c6b721540f
Kauã Alves <kauaalvesworkplace@gmail.com>
2022-10-21 08:54:44 -0300

    responsive problem solved

--- Src/controller/exercise1.js
+++ Src/controller/exercise1.js
@@ -1,3 +1,5 @@
+// ------ QuerySelectors ------
+
 const resultObject = document.querySelector("#result")
 const btnSubmit = document.querySelector('#btnSubmit')
 
@@ -6,6 +8,32 @@ const tolltipTriangle = document.querySelector('#tolltip-triangle')
 
 const objectInput1 =  document.querySelector("#n1")
 const objectInput2 = document.querySelector("#n2")
+
+// ------ functions ------
+
+function tollTipOn(){
+    tolltip.style.display = 'block'
+    tolltipTriangle.style.display = 'block'
+    setTimeout( ()=>{
+        tolltip.style.display = 'none'
+        tolltipTriangle.style.display = 'none'
+    }, 4000)
+    clearTimeout()
+    responsiveScreen(false);
+}
+function responsiveScreen(boolean){
+    
+    if(boolean == true){
+        if(window.screen.width >= 360 && window.screen.width <= 767){
+            document.querySelector('#section').style.height = '110vh';
+        }
+    }
+    if(boolean == false){
+        document.querySelector('#section').style.height = '91vh';
+    }
+}
+
+// ------ EventListeners ------
 
 btnSubmit.addEventListener('click', ()=>{
 
@@ -17,13 +45,7 @@ btnSubmit.addEventListener('click', ()=>{
     resultObject.innerText = "O resultado da sua soma é:   " + result;
 
 })
-
 resultObject.addEventListener('mouseover', ()=> {
-    tolltip.style.display = 'block'
-    tolltipTriangle.style.display = 'block'
-    setTimeout( ()=>{
-        tolltip.style.display = 'none'
-        tolltipTriangle.style.display = 'none'
-    }, 4000)
-    clearTimeout()
+    responsiveScreen(true);
+    tollTipOn()
 })
