e41862804f2c73a60fdee65e72c0e342a565820c
Kauã Alves <kauaalvesworkplace@gmail.com>
2022-11-05 22:52:05 -0300

    wrong answer bug solved

--- Src/controller/exercise2.js
+++ Src/controller/exercise2.js
@@ -7,41 +7,49 @@ const simpleTriangle = document.querySelector("#simpleTriangle")
 const resultObject = document.querySelector("#result")
 const btnClear = document.querySelector("#btn-clear")
 
+let btnOff = false
+
 // ------ EventListeners ------
 
 btnSubmit.addEventListener('click', ()=>{
     
-    let averageRate
-    const answer =  inputValues.value 
-    if(typeof answer != 'string'){
-        errorMassage();
-        return
+    if(btnOff == true){
+        return 
     }
-    try {
-        let answerArray = answer.split(',')
-        if(answerArray.length < 2){
+    else{
+        let averageRate
+        const answer =  inputValues.value 
+        btnOff = true
+        if(typeof answer != 'string'){
             errorMassage();
             return
         }
+        try {
+            let answerArray = answer.split(',')
+            if(answerArray.length < 2){
+                errorMassage();
+                return
+            }
 
-        let result = 0;
-        answerArray.forEach( item => {
-          result = result + eval(item)
-        })
-        averageRate = result/answerArray.length
-        console.log(averageRate);
+            let result = 0;
+            answerArray.forEach( item => {
+            result = result + eval(item)
+            })
+            averageRate = result/answerArray.length
 
-        resultObject.innerHTML = `Result: ${averageRate}`
-        resultObject.classList.add("result-right")
-        resultObject.style.display = "block"
+            resultObject.innerHTML = `Result: ${averageRate}`
+            resultObject.classList.add("result-right")
+            resultObject.style.display = "block"
 
-        btnClear.style.display = "block"
+            btnClear.style.display = "block"
 
-    } catch (error) { 
-        errorMassage(); 
-        console.error(error)
+        } catch (error) { 
+            errorMassage(); 
+            console.error(error)
+        }
+
     }
-
+    
 })
 
 btnClear.addEventListener("click", ()=> {
@@ -49,9 +57,14 @@ btnClear.addEventListener("click", ()=> {
 
     inputValues.value = null
     resultObject.innerHTML = 'x'
-    resultObject.classList.remove("result-right")
+
+    if(resultObject.classList[0] === 'result-wrong')  resultObject.classList.remove('result-wrong')
+    else resultObject.classList.remove('result-right')
+    
     resultObject.style.display = "none"
     btnClear.style.display = "none"
+
+    btnOff = false
 })
 
 thirdTittle.addEventListener('mouseover', ()=> {
